c0263e69c665751c0ba3f625c0118d8ce476a007
MichelaKalowski <mi.ky90@hotmail.com>
2023-02-14 15:37:33 +0100

    Update SingleBooks.jsx

--- src/components/SingleBooks.jsx
+++ src/components/SingleBooks.jsx
@@ -1,36 +1,28 @@
-import { Component } from "react";
+import React, { useState, useEffect } from "react";
 import { Card } from "react-bootstrap";
-//import CommentArea from "./CommentArea";
 
-class SingleBook extends Component {
-  // state = {
-  //  selected: false,
-  //};
+const SingleBook = (props) => {
+  const [selectedAsin, setSelectedAsin] = useState(props.selectedAsin);
 
-  render() {
-    return (
-      <>
-        <Card
-          //onClick={() => this.setState({ selected: !this.state.selected })}
-          onClick={() => this.props.changeSelectedAsin(this.props.book.asin)}
-          style={{
-            border:
-              this.props.selectedAsin === this.props.book.asin
-                ? "3px solid red"
-                : "none",
-          }}
-        >
-          <Card.Img variant="top" src={this.props.book.img} />
-          <Card.Body>
-            <Card.Title style={{ color: "black" }}>
-              {this.props.book.title}
-            </Card.Title>
-          </Card.Body>
-        </Card>
-        {/*this.state.selected && <CommentArea asin={this.props.book.asin} />*/}
-      </>
-    );
-  }
-}
+  useEffect(() => {
+    setSelectedAsin(props.selectedAsin);
+  });
+
+  return (
+    <>
+      <Card
+        onClick={() => props.changeSelectedAsin(props.book.asin)}
+        style={{
+          border: selectedAsin === props.book.asin ? "3px solid red" : "none",
+        }}
+      >
+        <Card.Img variant="top" src={props.book.img} />
+        <Card.Body>
+          <Card.Title style={{ color: "black" }}>{props.book.title}</Card.Title>
+        </Card.Body>
+      </Card>
+    </>
+  );
+};
 
 export default SingleBook;
